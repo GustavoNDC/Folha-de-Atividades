4c2cb47e6566b7a8aa4e3d4dc4e7d46e14de072f
GustavoNDC <gustavonb96@gmail.com>
2024-05-14 16:58:54 -0300

    FimAtvd1

--- Atividades/Atividade 1.js
+++ Atividades/Atividade 1.js
@@ -16,7 +16,7 @@ de dado de cada variável.*/
     let numInt = 10;
     let numDec = 3.14;
 
-    var mVar = (typeof numInt) +(" ")+ (typeof numDec);
+    var mVar = (typeof numInt) + (" ") + (typeof numDec) + (", Isso acontece porque independente se é inteiro ou decimal, javascript entende como 'number' ");
     document.getElementById("mQ6").innerText = mVar
 
 //    console.log("Tipo da variavel numInt:", typeof numInt);
@@ -26,10 +26,9 @@ de dado de cada variável.*/
 function Q8(){
     /*8. Escreva um programa que solicite ao usuário seu nome e exiba-o no console.*/
 
-    let readline = require('readline-sync');
+    var name8 = document.getElementById('nome8').value;
+    document.getElementById("mQ8").innerText = name8
 
-    let nome = readline.question("Digite seu nome: ");
-    console.log("Seu nome é: " + nome);
 
 }
 
@@ -37,22 +36,17 @@ function Q9 (){
     /*9. Escreva um programa que peça ao usuário para digitar sua idade e exiba-a no
     console.*/
 
-    let readline = require('readline-sync');
-
-    let idade = readline.question("Digite sua idade: ");
-    console.log("Sua idade é: " + idade);
+    var age9 = document.getElementById('idade9').value;
+    document.getElementById("mQ9").innerText = age9
 }
 
 function Q10 (){
     /*10.Escreva um programa que solicite ao usuário o seu nome e sua idade e em seguida
 imprima essas informações juntas em uma única linha.*/
 
-    let readline = require('readline-sync');
-
-    let nome = readline.question("Digite seu nome: ");
-    let idade = readline.question("Digite sua idade: ");
-
-    console.log("Seu nome é " + nome + " e você tem " + idade + " anos.");
+    var name10 = document.getElementById('nome10').value;
+    var age10 = document.getElementById('idade10').value;
+    document.getElementById("mQ10").innerText = ("Seu nome é " + name10 + " e você tem " + age10 + " anos.");
 
 }
 
@@ -60,12 +54,9 @@ function Q11 (){
     /*11.Escreva um programa que solicite ao usuário um número e converta-o para número
 usando parseInt(). Em seguida, exiba o número convertido no console.*/
 
-    let readline = require('readline-sync');
+    let numInt11 = parseInt(document.getElementById('num11').value);
+    document.getElementById("mQ11").innerText = ("Seu numero é : " + numInt11 + " , e o tipo da variavel é: " + typeof numInt11);
 
-    let numStr = readline.question("Digite um numero: ");
-    let numInt = parseInt(numStr);
-
-    console.log("Seu numeor é : " + numInt + " , e o tipo da variavel é: " + typeof numInt);
 }
 
 function Q12 (){
@@ -73,12 +64,7 @@ function Q12 (){
 converta-a para número de ponto flutuante usando parseFloat(). Em seguida, exiba
 a altura convertida no console.*/
 
-    let readline = require('readline-sync');
-
-    let alturaStr = readline.question("Dgite sua altura em metros: ");
-    let altura = parseFloat(alturaStr);
-
-    console.log("Sua altura é: " + altura + "m");
+    document.getElementById("mQ12").innerText = ("Sua altura em metros é: " + ((document.getElementById("altura12").value)/100) + "m");
 
 }
 
@@ -86,10 +72,7 @@ function Q13 (){
     /*13.Escreva um programa que solicite ao usuário sua idade e converta-a para
 número usando Number(). Em seguida, exiba a idade no console.*/
 
-    let readline = require('readline-sync');
+let idadeInt13 = parseInt(document.getElementById('idade13').value);
+document.getElementById("mQ13").innerText = ("Sua idade é : " + idadeInt13 + " , e o tipo da variavel é: " + typeof idadeInt13);
 
-    let idadeStr = readline.question("Digite sua idade: ");
-    let idade = Number(idadeStr);
-
-    console.log("Sua idade e: " + idade);
 }
